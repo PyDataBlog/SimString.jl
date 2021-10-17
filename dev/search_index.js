8b57d21dd580c0341894f8168aa3a2879187650a
Documenter.jl <documenter@juliadocs.github.io>
2021-10-17 22:01:44 +0000

    build based on da14c81

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SimString","category":"page"},{"location":"#SimString","page":"Home","title":"SimString","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SimString.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SimString]","category":"page"}]
+}
